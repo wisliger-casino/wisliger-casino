e11c58aec68dd729b212b57331ae5f358db4daf7
CORR1T <corsin@ragett.li>
2022-06-20 16:50:12 +0200

    We going Solid

--- wisliger-frontend/src/App.tsx
+++ wisliger-frontend/src/App.tsx
@@ -1,45 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Component, createSignal } from 'solid-js';
+import './App.scss';
+import Counter from './Counter';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: Component = () => {
+  const [counter, setCounter] = createSignal(0);
+  setInterval(setCounter, 1000, (c: number) => c + 1);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <>
+      <div>
+        <h1 class="header">{counter()}</h1>
+      </div>
+      <Counter />
+    </>
+  );
+};
 
-export default App
+export default App;
